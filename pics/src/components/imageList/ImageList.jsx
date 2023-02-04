e5a8afc34f40b0bd91c9ff81f58570991fd80612
code-xG9 <vy.code92@gmail.com>
2023-02-04 19:01:13 +0530

    Mapped all functionality with redux

--- pics/src/components/imageList/ImageList.jsx
+++ pics/src/components/imageList/ImageList.jsx
@@ -1,9 +1,22 @@
 import React, { Fragment } from "react";
 import ImageShow from "../imageShow/ImageShow";
-const ImageList = ({ data }) => {
-  const renderedImage = data.map((image) => {
-    return <ImageShow image={image} />;
-  });
-  return <Fragment>{renderedImage}</Fragment>;
+import { useSelector } from "react-redux";
+import Loader from "../loader/Loader";
+const ImageList = () => {
+  const { loading, results } = useSelector((state) => state.images);
+
+  const renderedImages =
+    results.length > 0 && !loading ? (
+      results.map((image) => {
+        return <ImageShow image={image} key={image} />;
+      })
+    ) : (
+      <Loader />
+    );
+  return (
+    <Fragment>
+      <div>{renderedImages}</div>
+    </Fragment>
+  );
 };
 export default ImageList;

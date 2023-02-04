a340173ac6abe40324359b90d79077c31f8a0589
code-xG9 <vy.code92@gmail.com>
2023-02-04 18:56:06 +0530

    All code is mapped based on API-running perfect

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,41 +1,19 @@
-import React, { Fragment, useEffect, useState } from "react";
-// import searchImage from "./api";
+import React, { Fragment, useState } from "react";
+import { searchImage } from "./utility/api";
 import SearchBar from "./components/searchBar/SearchBar";
 import ImageList from "./components/imageList/ImageList";
-import { useDispatch, useSelector } from "react-redux";
-import { searchImage } from "./redux/actions/GetImages";
 const App = () => {
   const [imagesStore, setImagesStore] = useState([]);
-
-  const dispatch = useDispatch();
-  const { loading, data, error } = useSelector((state) => state.images);
-
-  /**
-   * Code Implementation of API
-   */
-  /*
   const handleSubmit = async (term) => {
     console.log("Do a search with :", term);
     const results = await searchImage(term);
 
     setImagesStore(results);
-  }; 
-  */
-
-  /**
-   * Code Implementing logic with redux
-   */
-  const handleSubmit = (term) => {
-    dispatch(searchImage(term));
   };
-  useEffect(() => {
-    setImagesStore(data);
-  }, [dispatch, error]);
   return (
     <Fragment>
       <SearchBar onSubmit={handleSubmit} />
-      <ImageList />
-      {/* <ImageList data={imagesStore} /> */}
+      <ImageList data={imagesStore} />
     </Fragment>
   );
 };
--- pics/src/components/imageList/ImageList.jsx
+++ pics/src/components/imageList/ImageList.jsx
@@ -1,22 +1,9 @@
 import React, { Fragment } from "react";
 import ImageShow from "../imageShow/ImageShow";
-import { useDispatch, useSelector } from "react-redux";
-import Loader from "../loader/Loader";
-const ImageList = () => {
-  const { loading, results, error } = useSelector((state) => state.images);
-
-  const renderedImages =
-    results.length > 0 && !loading ? (
-      results.map((image) => {
-        return <ImageShow image={image} key={image} />;
-      })
-    ) : (
-      <Loader />
-    );
-  return (
-    <Fragment>
-      <div>{renderedImages}</div>
-    </Fragment>
-  );
+const ImageList = ({ data }) => {
+  const renderedImage = data.map((image) => {
+    return <ImageShow image={image} />;
+  });
+  return <Fragment>{renderedImage}</Fragment>;
 };
 export default ImageList;

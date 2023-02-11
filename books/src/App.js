613e41bc9805e5836ff516af5ad287b1283319a9
code-xG9 <vy.code92@gmail.com>
2023-02-11 17:00:12 +0530

    Completed the Form Hadlling task

--- books/src/App.js
+++ books/src/App.js
@@ -2,22 +2,22 @@ import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./app.scss";
 const App = () => {
-  return (
-    <div className='app'>
-      <div className='top'>dsfds</div>
-      <div className='main-page'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/'>
-              <Route index element={<Home />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <div className='bottom'>
-        <span>Copyright &copy; 2023-2025 - Books-Hub</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='top'>Books Hub Application</div>
+			<div className='main-page'>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/'>
+							<Route index element={<Home />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</div>
+			<div className='bottom'>
+				<span>Copyright &copy; 2023-2025 - Books-Hub</span>
+			</div>
+		</div>
+	);
 };
 export default App;
--- books/src/components/BookList.jsx
+++ books/src/components/BookList.jsx
@@ -1,15 +1,19 @@
 import BookShow from "./BookShow";
 import "./bookList.scss";
-const BookList = ({ books, onDelete }) => {
-  return (
-    <div className='bookList'>
-      {books &&
-        books.map((book) => (
-          <div className='newBook' key={book.id}>
-            <BookShow book={book} onDelete={onDelete} />
-          </div>
-        ))}
-    </div>
-  );
+const BookList = ({ books, onDelete, onEditClick }) => {
+	return (
+		<div className='bookList'>
+			{books &&
+				books.map((book) => (
+					<div className='newBook' key={book.id}>
+						<BookShow
+							book={book}
+							onDelete={onDelete}
+							onEditClick={onEditClick}
+						/>
+					</div>
+				))}
+		</div>
+	);
 };
 export default BookList;

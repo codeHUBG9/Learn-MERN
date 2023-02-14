0650c801eb9418dd5a015023e9c6e41dc84db8f1
code-xG9 <vy.code92@gmail.com>
2023-02-15 00:10:59 +0530

    Mapped all state using context

---
+++ books/src/context/books.js
@@ -0,0 +1,80 @@
+import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+const bookContext = createContext();
+
+function Provider({ children }) {
+	const [books, setBooks] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [showText, setShowText] = useState(false);
+	const [editFlag, setEditFlag] = useState(false);
+	const [editBook, setEditBook] = useState([]);
+
+	const fetchBooks = async () => {
+		const response = await axios.get("http://127.0.0.1:3350/books/");
+		setBooks(response.data);
+	};
+
+	const handleAdd = async (newBook) => {
+		const newId = uuidv4(); // Unique Id
+		newBook = { id: newId, ...newBook };
+		const response = await axios.post("http://127.0.0.1:3350/books", newBook);
+		const updatedBooks = [...books, response.data];
+		setBooks(updatedBooks);
+	};
+	const handleEdit = async (id, modifiedBook) => {
+		const response = await axios.put(
+			`http://127.0.0.1:3350/books/${id}`,
+			modifiedBook
+		);
+		const updatedBooks = books.map((book) => {
+			if (book.id === id) {
+				return { ...book, ...response.data };
+			}
+			return book;
+		});
+		setBooks(updatedBooks);
+		setEditFlag(false);
+	};
+	const handleDelete = async (id) => {
+		await axios.delete(`http://127.0.0.1:3350/books/${id}`);
+		const updatedBooks = books.filter((book) => {
+			return book.id !== id;
+		});
+		setBooks(updatedBooks);
+	};
+	const handleOpenClose = (flag) => {
+		setOpen(flag);
+		setShowText(false);
+	};
+
+	const onEditClick = async (id) => {
+		const response = await axios.get(`http://127.0.0.1:3350/books/${id}`);
+		setEditBook(response.data);
+		setOpen(true);
+		setEditFlag(true);
+	};
+
+	return (
+		<bookContext.Provider
+			value={{
+				fetchBooks,
+				handleAdd,
+				handleDelete,
+				handleOpenClose,
+				onEditClick,
+				handleEdit,
+				open,
+				showText,
+				editFlag,
+				editBook,
+				setOpen,
+				setShowText,
+				books,
+			}}>
+			{children}
+		</bookContext.Provider>
+	);
+}
+export { Provider };
+export default bookContext;
